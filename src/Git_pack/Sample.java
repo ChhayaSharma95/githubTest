11ab49f884360bf205f790f155b283365f649da1
ChhayaSharma95 <chhayasharma0495@gmail.com>
2022-06-19 01:07:08 +0530

    adding project

---
+++ src/Git_pack/Sample.java
@@ -0,0 +1,13 @@
+package Git_pack;
+
+public class Sample {
+		public static void  main(String[]arg) {
+			String s="Hello";
+			String rev=" ";
+			for(int i=s.length()-1;i>=0;i--) {
+				rev=rev+s.charAt(i);
+			}
+			System.out.print(rev);
+	}}
+
+
