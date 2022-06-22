507a7e00acc42c3149cae44de12a698c7f0d531d
ChhayaSharma95 <chhayasharma0495@gmail.com>
2022-06-23 00:06:55 +0530

    adding test class

---
+++ Git_pack/Sample.java
@@ -0,0 +1,24 @@
+package Git_pack;
+
+
+//make changes
+public class Sample {
+		public static void  main(String[]arg) {
+			String s="Hello";
+			String rev=" ";
+			for(int i=s.length()-1;i>=0;i--) {
+				rev=rev+s.charAt(i);
+			}
+			System.out.print(rev);
+	}}
+
+class Demo{
+	public static void main(String[]arg) {
+		int a=4;
+		int b=4;
+		int c=a+b;
+		System.out.print(c);
+	}
+}
+
+
---
+++ Git_pack/Test.java
@@ -0,0 +1,9 @@
+package Git_pack;
+
+public class Test {
+	public static void main(String[]arg) {
+		int a=4;
+		int b=4;
+		int c=a*b;
+		System.out.print(c);
+}}
