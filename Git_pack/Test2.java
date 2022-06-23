67fc179206bdcce8ba8f779749f83aaa89173583
ChhayaSharma95 <chhayasharma0495@gmail.com>
2022-06-23 23:35:48 +0530

    adding cloud class

--- Git_pack/Test2.java
+++ Git_pack/Test2.java
@@ -2,6 +2,9 @@ package Git_pack;
 
 public class Test2 {
 	public static void main(String[]arg) {
+
+		String str="hello";
+
     System.out.println("hello");
 }
 }
---
+++ Git_pack/salesforce.java
@@ -0,0 +1,11 @@
+package Git_pack;
+
+public class salesforce {
+	public static void main(String[]arg) {
+		char a='B';
+		char b='C';
+		System.out.println(a+b);
+		
+	}
+
+}
